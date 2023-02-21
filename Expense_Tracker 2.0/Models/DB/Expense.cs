9d7076481fea4fbe32792b9c1b5921cc02688af6
Nikolay <nikolay.zlatev2005@gmail.com>
2023-02-21 21:12:05 +0200

    creating folders Models and DB, two classes which are template for my tables

---
+++ Expense_Tracker 2.0/Models/DB/Expense.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker_2._0.Models.DB
+{
+    public class Expense
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Type { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Date { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public decimal Amount { get; set; }
+
+
+        //one to many relationship
+
+        [ForeignKey(nameof(User))] //attribute  
+        public int UserId { get; set; } //FK
+        public User User { get; set; } //navigational property
+    }
+}
---
+++ Expense_Tracker 2.0/Models/DB/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Expense_Tracker_2._0.Models.DB
+{
+    public class User
+    {
+        //[Required] - not null
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)] // this is like VarChar(50)
+        public string UserName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Password { get; set; }
+
+        //this can be null
+        [MaxLength(50)]
+        public string? Email { get; set; }
+
+        //one to many
+        public ICollection<Expense> Expenses { get; set; }
+    }
+}
