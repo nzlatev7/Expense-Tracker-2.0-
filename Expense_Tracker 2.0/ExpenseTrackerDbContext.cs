a953f861553ab7dbd1d80e1815c296cd8484c3d6
Nikolay <nikolay.zlatev2005@gmail.com>
2023-02-24 23:28:58 +0200

    creating tables and solving problem with fluent Api(nullable problem with UserId)

---
+++ Expense_Tracker 2.0/ExpenseTrackerDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Expense_Tracker_2._0.Models.DB;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker_2._0
+{
+    public class ExpenseTrackerDbContext : DbContext
+    {    
+        public ExpenseTrackerDbContext(DbContextOptions<ExpenseTrackerDbContext> options) : base(options) { }
+
+        //tables
+        public DbSet<User> Users { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Expense>()
+                .Property(b => b.UserId)
+                .IsRequired(false);
+        }
+        //fluent API???
+    }
+}
