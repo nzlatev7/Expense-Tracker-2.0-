776d074860ff1472f1668d70667b9ecdf00fd778
Nikolay <nikolay.zlatev2005@gmail.com>
2023-02-25 11:25:32 +0200

    solving nullable problem with UserId

---
+++ Expense_Tracker 2.0/Migrations/20230224204739_solvingUserIdToNotRequired.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expense_Tracker_2._0.Migrations
+{
+    public partial class solvingUserIdToNotRequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_Users_UserId",
+                table: "Expenses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Expenses",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_Users_UserId",
+                table: "Expenses",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_Users_UserId",
+                table: "Expenses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Expenses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_Users_UserId",
+                table: "Expenses",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
