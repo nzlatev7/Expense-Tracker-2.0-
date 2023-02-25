017f6e17d58ba923ed064e204c6cba409fd11a70
Nikolay <nikolay.zlatev2005@gmail.com>
2023-02-25 21:57:22 +0200

    making the GetAll model for the expenses

---
+++ Expense_Tracker 2.0/Models/Response/ExpenseGetAllResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Expense_Tracker_2._0.Models.Response
+{
+    public class ExpenseGetAllResponse
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Date { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
