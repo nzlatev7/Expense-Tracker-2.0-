379107df02f59c88fbe4b9e59eb53b681a403c49
Nikolay <nikolay.zlatev2005@gmail.com>
2023-03-02 22:27:44 +0200

    making UserRegisterRequest and UserUpdateRequest models

---
+++ Expense_Tracker 2.0/Models/Request/UserRegisterRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Expense_Tracker_2._0.Models.Request
+{
+    public class UserRegisterRequest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string? Email { get; set; }
+    }
+}
---
+++ Expense_Tracker 2.0/Models/Request/UserUpdateRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Expense_Tracker_2._0.Models.Request
+{
+    public class UserUpdateRequest
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string? Email { get; set; }
+    }
+}
