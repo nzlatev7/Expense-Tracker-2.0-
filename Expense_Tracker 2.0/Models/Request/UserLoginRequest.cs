67732bcefda29eeebc7510de807f6916d269f93e
Nikolay <nikolay.zlatev2005@gmail.com>
2023-03-03 13:58:37 +0200

    making the UserLoginRequest

---
+++ Expense_Tracker 2.0/Models/Request/UserLoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Expense_Tracker_2._0.Models.Request
+{
+    public class UserLoginRequest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
