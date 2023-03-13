3494338e780fb1ae5bafd3961672885bce7936ec
BorisMatev <boris.matev72345@gmail.com>
2023-03-13 23:13:05 +0200

    User service changes

--- Expense_Tracker 2.0/ClientApp/src/app/services/user.service.ts
+++ Expense_Tracker 2.0/ClientApp/src/app/services/user.service.ts
@@ -14,4 +14,23 @@ export class UserService {
     return this.http.post(`${this.url}/User/Register`, body);
   }
 
+  logIn(body: any){
+    return this.http.post(`${this.url}/User/Login`, body);
+  }
+
+  update(body: any){
+    return this.http.put(`${this.url}/User/Update`, body);
+  }
+
+  getAll(){
+    return this.http.get(`${this.url}/User/GetAll`);
+  }
+
+  deleteItem(id: any) {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+    return this.http.delete(`${this.url}/User/Delete`, { headers: headers, body: id });
+  }
+
 }
