a45fd23e14cf5d8646e28f53d2118afb47a45a96
Nikolay <nikolay.zlatev2005@gmail.com>
2023-04-05 23:53:17 +0300

    Make the EmailVlidation when a user try to Register. Using Regex

--- Expense_Tracker 2.0/Controllers/UserController.cs
+++ Expense_Tracker 2.0/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using Expense_Tracker_2._0.Models.Request;
 using Expense_Tracker_2._0.Models.Response;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
 
 namespace Expense_Tracker_2._0.Controllers
 {
@@ -24,11 +25,29 @@ namespace Expense_Tracker_2._0.Controllers
             User user = new User();
             user.UserName = request.UserName;
             user.Password = request.Password;
-            user.Email = request.Email;
 
+            user.Email = EmailValidation(request.Email) ? user.Email = request.Email : null;
+
+            if (user.Email == null)
+            {
+                return BadRequest("Invalid Email");
+            }    
+            
             _dbContext.Users.Add(user);
             _dbContext.SaveChanges();
             return Ok();
+        }
+        static bool EmailValidation(string email)
+        {
+            string emialPattern = @"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$";
+
+            Match match = Regex.Match(email, emialPattern);
+
+            if (match.Success)
+            {
+                return true;
+            }
+            return false;
         }
 
         [HttpPost]
