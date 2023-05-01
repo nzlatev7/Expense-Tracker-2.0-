9e2fac7e7072d463e61bec679724160309c903b9
Nikolay <nikolay.zlatev2005@gmail.com>
2023-05-01 11:44:33 +0300

    change the migrations because there is a building eror

---
+++ Expense_Tracker 2.0/Migrations/20230501074903_addingUserRole.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expense_Tracker_2._0.Migrations
+{
+    public partial class addingUserRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "Users",
+                type: "int",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+        }
+    }
+}
