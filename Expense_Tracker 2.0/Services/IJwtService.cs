b5dfc22d3f06ca2080d64fa28751c44634fde2c0
Nikolay <nikolay.zlatev2005@gmail.com>
2023-05-02 17:38:35 +0300

    making the jwt service

---
+++ Expense_Tracker 2.0/Services/IJwtService.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+
+namespace Expense_Tracker_2._0.Services
+{
+    public interface IJwtService
+    {
+        int GetUserIdFromToken(ClaimsPrincipal user);
+    }
+}
---
+++ Expense_Tracker 2.0/Services/JwtService.cs
@@ -0,0 +1,13 @@
+﻿using Expense_Tracker_2._0.Models.DB;
+using System.Security.Claims;
+
+namespace Expense_Tracker_2._0.Services
+{
+    public class JwtService : IJwtService
+    {
+        public int GetUserIdFromToken(ClaimsPrincipal user)
+        {
+            return int.Parse(user.Claims.First(i => i.Type == ClaimTypes.NameIdentifier).Value);
+        }
+    }
+}
