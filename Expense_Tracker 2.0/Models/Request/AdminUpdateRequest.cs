a27f076d3d320048fcd2998f582b252d441ae2c0
Nikolay <nikolay.zlatev2005@gmail.com>
2023-05-06 12:07:34 +0300

    delete the uderId in Update model and adding adminRequests

---
+++ Expense_Tracker 2.0/Models/Request/AdminUpdateRequest.cs
@@ -0,0 +1,13 @@
+﻿using Expense_Tracker_2._0.Models.DB;
+
+namespace Expense_Tracker_2._0.Models.Request
+{
+    public class AdminUpdateRequest
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+        public string? Email { get; set; }
+    }
+}
