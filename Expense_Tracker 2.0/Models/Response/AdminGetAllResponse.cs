81fd42d7c81ffa8e522d307ad36293f2d5f62c32
Nikolay <nikolay.zlatev2005@gmail.com>
2023-05-06 12:08:54 +0300

    update the userGetAllResponse to userGetInfo and adding admingGetAllResponse

---
+++ Expense_Tracker 2.0/Models/Response/AdminGetAllResponse.cs
@@ -0,0 +1,16 @@
+﻿using Expense_Tracker_2._0.Models.DB;
+
+namespace Expense_Tracker_2._0.Models.Response
+{
+    public class AdminGetAllResponse
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+        public string? Email { get; set; }
+
+        //in the future we will yield return this expenses
+        //public List<Expense> Expenses { get; set; }
+    }
+}
--- Expense_Tracker 2.0/Models/Response/UserGetInfoResponse.cs
+++ Expense_Tracker 2.0/Models/Response/UserGetInfoResponse.cs
@@ -2,12 +2,13 @@
 
 namespace Expense_Tracker_2._0.Models.Response
 {
-    public class UserGetAllResponse
+    public class UserGetInfoResponse
     {
         public int Id { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
         public string? Email { get; set; }
-        public ICollection<Expense> Expenses { get; set; }
+
+        //public List<Expense> Expenses { get; set; } - this will happend in the Expense functionality
     }
 }
