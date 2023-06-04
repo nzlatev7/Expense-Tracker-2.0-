c2ef94ab2d50686b7272939d16fb07a07e35cfc7
Nikolay <nikolay.zlatev2005@gmail.com>
2023-06-04 15:24:59 +0300

    validate the pageNumber in GetAllStepByStep, Admin

--- Expense_Tracker 2.0/Controllers/AdminController.cs
+++ Expense_Tracker 2.0/Controllers/AdminController.cs
@@ -51,9 +51,14 @@ namespace Expense_Tracker_2._0.Controllers
         }
 
         [HttpGet]
-        public List<AdminGetAllResponse> GetAllStepByStep(int pageNumber)
+        public ActionResult<List<AdminGetAllResponse>> GetAllStepByStep(int pageNumber)
         {
-            return _dbContext.Users
+            if (pageNumber <= 0)
+            {
+                return BadRequest("Page number must be a positive integer.");
+            }
+
+            var users = _dbContext.Users
                 .Skip((pageNumber - 1) * 10)
                 .Take(10)
                 .Select(x => new AdminGetAllResponse()
@@ -64,13 +69,14 @@ namespace Expense_Tracker_2._0.Controllers
                     Email = x.Email,
                 }).ToList();
 
+            return Ok(users);
+
             // implement pagination using (skip, take)
 
             // another way is to use 'yield return' which is a good choice
             // when we retrieve a big amount of data, lazy loading and streaming effect
             // here is more practical to use (skip,take),
             // bacause we can have 'RANDOM ACCESS' - jump to a particular page
-
         }
 
         [HttpDelete]
