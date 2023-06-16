fdd7f3653f794a59af9a313e8bf39ff64f047871
Nikolay <nikolay.zlatev2005@gmail.com>
2023-06-16 11:15:33 +0300

    update some response properties

--- Expense_Tracker 2.0/Models/Response/AdminGetAllUsersResponse.cs
+++ Expense_Tracker 2.0/Models/Response/AdminGetAllUsersResponse.cs
@@ -2,15 +2,12 @@
 
 namespace Expense_Tracker_2._0.Models.Response
 {
-    public class AdminGetAllResponse
+    public class AdminGetAllUsersResponse
     {
         public int Id { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
         public Role Role { get; set; }
         public string? Email { get; set; }
-
-        //in the future we will yield return this expenses
-        //public List<Expense> Expenses { get; set; }
     }
 }
