8b7d142859ff2315bd5da6b7f6a552f13714b377
Nikolay <nikolay.zlatev2005@gmail.com>
2023-07-11 19:31:54 +0300

    creating validationToken table

---
+++ Expense_Tracker 2.0/Models/DB/ValidationToken.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Expense_Tracker_2._0.Models.DB
+{
+    public class ValidationToken
+    {
+        [Key]
+        public string Token { get; set; }
+        public DateTime ExpirationDate { get; set; }
+
+        public int UserId { get; set; }
+
+        public int AttemptsCount { get; set; }
+    }
+}
