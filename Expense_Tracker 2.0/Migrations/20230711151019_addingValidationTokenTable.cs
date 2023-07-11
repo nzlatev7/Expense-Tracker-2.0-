f6c82ca77643b3bccbd9b2e89a692ff966c22b88
Nikolay <nikolay.zlatev2005@gmail.com>
2023-07-11 19:32:32 +0300

    adding migrations

---
+++ Expense_Tracker 2.0/Migrations/20230711151019_addingValidationTokenTable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expense_Tracker_2._0.Migrations
+{
+    public partial class addingValidationTokenTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ValidationTokens",
+                columns: table => new
+                {
+                    Token = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ExpirationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    AttemptsCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ValidationTokens", x => x.Token);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ValidationTokens");
+        }
+    }
+}
