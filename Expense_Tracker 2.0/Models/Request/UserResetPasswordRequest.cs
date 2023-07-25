051f41af97838af095c8e480a80c9fbec341f540
Nikolay <nikolay.zlatev2005@gmail.com>
2023-07-25 10:13:10 +0300

    models for reset password requests

---
+++ Expense_Tracker 2.0/Models/Request/UserResetPasswordRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Expense_Tracker_2._0.Models.Request
+{
+    public class UserResetPasswordRequest
+    {
+        public string Email { get; set; }
+    }
+}
---
+++ Expense_Tracker 2.0/Models/Request/UserVerifyResetPasswordToken.cs
@@ -0,0 +1,9 @@
+﻿namespace Expense_Tracker_2._0.Models.Request
+{
+    public class UserVerifyResetPasswordToken
+    {
+        public string TokenValue { get; set; }
+
+        public string NewPassword { get; set; }
+    }
+}
